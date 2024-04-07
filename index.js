3e5ac55469cbdff8118c05fb15a0ccbf2393c07c
Tom Bangle <tombangle@gmail.com>
2024-04-06 22:44:26 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+    if (distance === 199) {
+      return `This one is on me!`
+    } else if(distance === 1500){
+          return `That will be twenty bucks.`
+    } else if(distance === 2001){
+            return `I will gladly take your thirty bucks.`
+    } else if(distance === 2501){
+            return `No can do.`
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+    return city === `NYC`?`Ok, sounds good.` :`No go.`
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+    switch(tip){
+        case `generous`:
+            return `Thank you so much.`
+        case `not as generous`:
+            return `Thank you.`
+        default:
+            return `Bye.`
+    }
 }
